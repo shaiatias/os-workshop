f035556b35df0f4b7f60406e2908fccd0b0cfcb8
shai@experitest.com <shai@experitest.com>
2017-12-05 00:18:12 +0200

    written all code

--- hello.c
+++ hello.c
@@ -6,15 +6,70 @@ MODULE_LICENSE("MIT");
 MODULE_AUTHOR("SHAI ATIAS");
 MODULE_DESCRIPTION("Simple hello world module");
 
+#define NULL = 0;
+
+typedef struct Node {
+	int info;
+	Node* next;
+}
+
+static Node* root = NULL;
+
+static void print_list()
+{
+	Node* item = root;
+
+	while (item != NULL) {
+		printk(KERN_INFO "printing item %d \n", item->info);
+		item = item->next;
+	} 
+}
+
+static void remove_from_list()
+{
+	Node* item = root;
+	
+	if (item->next == NULL)
+	{
+		return;
+	}
+
+	while (item->next != NULL) {
+		item = item->next;
+	}
+
+	item->next = NULL;
+}
+
 static int __init hello_init(void)
 {
 	printk(KERN_INFO "hello from shai.\n");
+
+	root = malloc(sizeof(Node));
+	root->info = 1;
+
+	root->next = malloc(sizeof(Node));
+	root->next->info = 2;
+	
+	root->next->next = malloc(sizeof(Node));
+	root->next->next->info = 3;
+
+	root->next->next->next = malloc(sizeof(Node));
+	root->next->next->next->info = 4;
+
+	root->next->next->next->next = NULL;
+
+	print_list();
+
 	return 0;
 }
 
 static void __exit hello_cleanup(void)
 {
 	printk(KERN_INFO "cleanup work\n");
+	
+	remove_from_list();
+	print_list();
 }
 
 module_init(hello_init);
