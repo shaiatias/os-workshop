868302d78c2d930b8d7ae6c131acd0cc59b28e19
shai@experitest.com <shai@experitest.com>
2017-12-05 01:01:31 +0200

    missing struct

--- hello.c
+++ hello.c
@@ -7,60 +7,58 @@ MODULE_LICENSE("MIT");
 MODULE_AUTHOR("SHAI ATIAS");
 MODULE_DESCRIPTION("Simple hello world module");
 
-#define NULL = 0;
-
 typedef struct Node {
 	int info;
 	struct Node *next;
+} Node;
+
+static Node root = { 1, NULL };
+
+void print_list(Node *root)
+{
+	// Node *item = root;
+
+	// while (item != NULL) {
+	// 	printk(KERN_INFO "printing item %d \n", item->info);
+	// 	item = item->next;
+	// } 
 }
 
-static Node *root = NULL;
-
-static void print_list()
+void remove_from_list(Node *root)
 {
-	Node *item = root;
-
-	while (item != NULL) {
-		printk(KERN_INFO "printing item %d \n", item->info);
-		item = item->next;
-	} 
-}
-
-static void remove_from_list()
-{
-	Node *item = root;
+	// Node *item = root;
 	
-	if (item->next == NULL)
-	{
-		return;
-	}
+	// if (item->next == NULL)
+	// {
+	// 	return;
+	// }
 
-	while (item->next != NULL) {
-		item = item->next;
-	}
+	// while (item->next != NULL) {
+	// 	item = item->next;
+	// }
 
-	item->next = NULL;
+	// item->next = NULL;
 }
 
 static int __init hello_init(void)
 {
 	printk(KERN_INFO "hello from shai.\n");
 
-	root = kmalloc(sizeof(Node));
+	root = kmalloc(sizeof(Node), GFP_KERNEL);
 	root->info = 1;
 
-	root->next = kmalloc(sizeof(Node));
-	root->next->info = 2;
+	// root->next = kmalloc(sizeof(Node));
+	// root->next->info = 2;
 	
-	root->next->next = kmalloc(sizeof(Node));
-	root->next->next->info = 3;
+	// root->next->next = kmalloc(sizeof(Node));
+	// root->next->next->info = 3;
 
-	root->next->next->next = kmalloc(sizeof(Node));
-	root->next->next->next->info = 4;
+	// root->next->next->next = kmalloc(sizeof(Node));
+	// root->next->next->next->info = 4;
 
-	root->next->next->next->next = NULL;
+	// root->next->next->next->next = NULL;
 
-	print_list();
+	// print_list();
 
 	return 0;
 }
@@ -69,8 +67,8 @@ static void __exit hello_cleanup(void)
 {
 	printk(KERN_INFO "cleanup work\n");
 	
-	remove_from_list();
-	print_list();
+	// remove_from_list();
+	// print_list();
 }
 
 module_init(hello_init);
